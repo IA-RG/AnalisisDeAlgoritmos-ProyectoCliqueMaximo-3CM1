e0a791638cee30d38473482478fa1030a6f77788
TheClownFiesta <alexgamesrg@gmail.com>
2020-07-12 21:04:31 -0500

    Proyecto final clique máximo 3CM1
    Programa desarrollado en netbeans que implementa un algorimo para resolver un problema de optimización combinatoria NP aplicado a grafos por medio de nodos y pesos.

---
+++ Netbeans_ProyectoCliqueMáximo/src/Ventanas/Aristas.java
@@ -0,0 +1,15 @@
+
+package Ventanas;
+
+
+public class Aristas {
+    int j,k;
+
+    Aristas(int j, int k) {
+        this.j=j;
+        this.k=k;
+        
+//Para cambiar el cuerpo de los métodos generados, elija Tools | Templates.
+    }
+    
+}
